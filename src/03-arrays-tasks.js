d903624c2cae2624e92d7aec3239ef785b542f3c
Tornike Machitidze <tornike.machitidze7@gmail.com>
2021-09-29 22:06:50 +0400

    one more step

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -207,8 +207,9 @@ function getTail(arr, n) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(/* arr */) {
-  throw new Error('Not implemented');
+function toCsvText(arr) {
+  let res = arr.reduce((res, el) => res += el.join() + '\n', '');
+  return res.slice(0, -1);
 }
 
 /**
